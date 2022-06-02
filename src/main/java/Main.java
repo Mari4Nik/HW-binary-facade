31c2f158d5c16f6405f04839713dff7dd37c9994
Mari4Nik <ma_nikiforova@mail.ru>
2022-06-02 12:23:04 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10", "100"));
+        System.out.println(bins.mult("100", "100"));
+    }
+}
